8d0260cb64ea08c00689eea910b85b87ba08e754
Adam Breaux <breauxa@email.chop.edu>
2016-06-13 09:40:19 -0400

    deal with the divergences

--- manifests/params.pp
+++ manifests/params.pp
@@ -9,7 +9,26 @@ class mongodb::params {
   $mongodb_package_ensure = 'latest'
   
   case $::operatingsystem {
-    'CentOS', 'Debian', 'OracleLinux', 'RedHat': {
+    'CentOS', 'OracleLinux', 'RedHat': {
+      $mongodb_config  = '/etc/mongodb.conf'
+      $mongodb_package = 'mongodb-server'
+      $mongodb_service = 'mongod'
+    
+      group { 'mongodb':
+        ensure => present,
+        gid    => 184,
+      }
+    
+      user { 'mongodb':
+        ensure     => present,
+        gid        => 184,
+        home       => '/var/lib/mongodb',
+        shell      => '/bin/false',
+        managehome => true,
+        uid        => 184,
+      }
+    }
+    'Debian': {
       $mongodb_config  = '/etc/mongodb.conf'
       $mongodb_package = 'mongodb-server'
       $mongodb_service = 'mongodb'
